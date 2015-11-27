e39782b18f9e3121c9511d3a39dfd30d2e7080f6
Fabio Tisci <fabio.tapia@ext.soprasteria.com>
2015-11-27 12:08:53 +0000

    structure with gulp working

---
+++ src/app/app.module.js
@@ -0,0 +1,51 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('cpp-ui-spa-master', [
+            'cpp-ui-spa-master.config',
+            'ui.bootstrap',
+            'ui.bootstrap',
+            'ui.cpp',
+            'angularUtils.directives.dirPagination',
+            'ui.router',
+            'ngAnimate',
+            'ngSanitize',
+            'ngLocalize',
+            'ngLocalize.Config',
+            'ngLocalize.Events',
+            'ngLocalize.InstalledLanguages',
+            'ngCookies',
+            'permission',
+            'angular-ladda',
+            'angular-underscore'
+        ])
+        .run(runBlock);
+
+    function runBlock($rootScope, locale) {
+      $rootScope.langs = [{
+        value: 'en-GB',
+        label: 'English'
+      }, {
+        value: 'cy',
+        label: 'Cymraeg'
+      }];
+
+      // Language Select Function
+      $rootScope.selectedLanguage = $rootScope.langs[_.findIndex($rootScope.langs, {value: locale.getLocale()})];
+
+      $rootScope.updadeLang = function(lang) {
+        locale.setLocale(lang.value);
+      };
+
+      $rootScope.globalNav = {
+        pageTitle: {
+          title: 'Page title',
+          type: 'text',
+          ref: '#'
+        }
+      };
+
+    }
+
+}());
---
+++ src/app/config/config-global.js
@@ -0,0 +1,14 @@
+(function () {
+	'use strict';
+
+	/**
+	 * @description
+	 * This module houses local configuration settings belonging to the deployment,
+	 * and is dynamically generated.
+	 */
+
+	angular
+		.module('cpp-ui-spa-master.config.global', [])
+	  	.constant('ENV', 'development');
+
+}());
