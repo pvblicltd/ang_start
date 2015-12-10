aadb77dd44c12cea2649b7333c2971becc0b1acb
Olu Alli <alleypa@yahoo.com>
2015-12-10 20:37:02 +0000

    Added node server for raml mocks

--- mock/server.js
+++ mock/server.js
@@ -1,1 +1,34 @@
-﻿
+﻿'use strict';
+
+var app = require('express')(),
+    errorHandler = require('./utils/error-handler.js')(),
+    fs = require('fs'),
+    host = 'http://localhost:' || '', // TODO: make dynamic
+    logger = require('morgan'),
+    path = require('path'),
+    ramlServer = require('raml-mocker-server'),
+    yamlConfig = require('node-yaml-config');
+
+
+var config = yamlConfig.load(path.join(__dirname, '/config/config.yml'));
+
+if (!fs.existsSync(path.join(__dirname, config.scheduling.path))) {
+    throw new Error('Project path ' + config.scheduling.path + ' does not exist');
+}
+
+app.use(logger('dev'));
+app.use(errorHandler.init);
+
+var options = {
+    debug: true,
+    port: config.scheduling.port,
+    path: config.scheduling.path,
+    watch: true
+}
+
+// load all endpoints
+require('./endpoints/hearing')(app, ramlServer, options);
+
+app.listen(config.scheduling.port, function () {
+    console.log('The CSSAPI Raml mock server is running at ' + host + config.scheduling.port);
+});
